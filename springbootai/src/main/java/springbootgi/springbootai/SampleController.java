aea8f57f44d35161b21b1ee00c0480d7763ec7e7
조민서 <msjo8@lotte.net>
2019-07-08 13:17:44 +0900

    thymeleaf test

--- springbootai/src/main/java/springbootgi/springbootai/SampleController.java
+++ springbootai/src/main/java/springbootgi/springbootai/SampleController.java
@@ -2,8 +2,12 @@ package springbootgi.springbootai;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import thymeleaf.User;
 
 @Controller
 @EnableAutoConfiguration
@@ -15,6 +19,14 @@ public class SampleController {
 
         return "Hello Spring Boot!!!test";
 
+    }
+
+	@GetMapping("/test")
+    public String getUser(Model model) {
+		// https://eblo.tistory.com/54
+        User user = new User("kkaok", "테스트", "web") ;
+        model.addAttribute("user", user);
+        return "test";
     }
 
 }
---
+++ springbootai/src/main/java/thymeleaf/UserTestController.java
@@ -0,0 +1,16 @@
+package thymeleaf;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserTestController {
+
+//    @GetMapping("/test")
+//    public String getUser(Model model) {
+//        User user = new User("kkaok", "테스트", "web") ;
+//        model.addAttribute("user", user);
+//        return "test";
+//    }
+}
